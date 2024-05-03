f91bb9ccb28899457b41415ae4cbef6c3cfa2d34
theshanumalik <shanumalik9001567.com>
2024-05-03 18:57:36 +0530

    firebase initialized

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,10 +2,10 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAH7fqs96QZKCTWb-jlZbelN7JE0RcdwwM",
-  authDomain: "subodhians-b2398.firebaseapp.com",
-  projectId: "subodhians-b2398",
-  storageBucket: "subodhians-b2398.appspot.com",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: "349225959312",
   appId: "1:349225959312:web:6cfdd5839d8a263f0ffd15",
   measurementId: "G-V4QT0YSL32",
